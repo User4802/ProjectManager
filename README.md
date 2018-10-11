eb9bb9cca17179bb26041dce93d8edf8650c0256
user4802 <24318149+User4802@users.noreply.github.com>
2018-10-11 15:36:24 -0400

    initial

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # projectView
+
+# query user for the init method view / add
+    ## admin can bypass by typing admin here
+        ### admin can delete, modify, force add
+# if view display the current DB
+# if add ask the following
+    ## Job Name *
+    ## General Contractor *
+    ## postal code of worksite * (needed to not get duplicate)
+    ## due date (not mandatory)
--- projectView_0.2.py
+++ projectView_0.2.py
@@ -1,14 +1,4 @@
-# query user for the init method view / add
-    ## admin can bypass by typing admin here
-        ### admin can delete, modify, force add
-# if view display the current DB
-# if add ask the following
-    ## Job Name *
-    ## General Contractor *
-    ## postal code of worksite * (needed to not get duplicate)
-    ## due date (not mandatory)
-
-version = "beta_v0.2"
+version = "beta_v0.3"
 module = "ProjectView"
 spacer = len(module) * "-"
 splash = "\n{} {} {} {}\n".format(spacer,module,version,spacer)
